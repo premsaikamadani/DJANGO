40268c40c57e8bb538e0be9b15c8fe0f15233fc0
premsaikamadani <saikamadani148@gmail.com>
2026-01-11 19:22:37 +0530

    task3 complete

--- ipl_project/ipl_app/views.py
+++ ipl_project/ipl_app/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Welcome to the IPL App 🏏")
+
+def register(request):
+    return render(request, 'register.html')
