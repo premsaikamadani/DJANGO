b37945201bf0735970e94610ee9e2e9e13ee7373
premsaikamadani <saikamadani148@gmail.com>
2026-01-13 21:19:02 +0530

    models create

--- ipl_project/ipl_app/models.py
+++ ipl_project/ipl_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Franchise(models.Model):
+    name = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=10)
+    founded_year = models.IntegerField()
+    no_of_trophies = models.IntegerField()
+    logo = models.ImageField(blank=True, null=True)
+    city = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    coach = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'franchises'
+
+    def __str__(self):
+        return f"{self.name} ({self.short_name})"
