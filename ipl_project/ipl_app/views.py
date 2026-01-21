79dfd6bf1338ea00e0b834a521931ede63ec11d7
premsaikamadani <saikamadani148@gmail.com>
2026-01-21 21:18:24 +0530

    changes complete

--- ipl_project/ipl_app/views.py
+++ ipl_project/ipl_app/views.py
@@ -1,28 +1,58 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Franchise
+
 
 def home(request):
     return HttpResponse("Welcome to the IPL App 🏏")
 
+
 def register(request):
     if request.method == 'POST':
-        # Here you would handle the form submission, e.g., save user data
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirm_password')
+
         print(username, email, password, confirm_password)
         return HttpResponse("Registration successful!")
     else:
         return render(request, 'register.html')
-    
+
 
 def login_view(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        print(username, password)  # should NOT be None now
+        print(username, password)
         return HttpResponse("Login successful!")
     else:
         return render(request, 'login.html')
+
+
+def register_franchise(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        short_name = request.POST.get('short_name')
+        founded_year = request.POST.get('founded_year')
+        no_of_trophies = request.POST.get('no_of_trophies')
+        city = request.POST.get('city')
+        owner = request.POST.get('owner')
+        coach = request.POST.get('coach')
+
+        print(name, short_name, founded_year, no_of_trophies, city, owner, coach)
+
+        Franchise.objects.create(
+            name=name,
+            short_name=short_name,
+            founded_year=founded_year,
+            no_of_trophies=no_of_trophies,
+            city=city,
+            owner=owner,
+            coach=coach
+        )
+
+        return HttpResponse("Franchise registered successfully!")
+    else:
+        return render(request, 'register_franchise.html')
--- ipl_project/ipl_project/urls.py
+++ ipl_project/ipl_project/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from ipl_app import views   # ✅ CORRECT IMPORT
+from ipl_app import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register, name='register'),
     path('ipl/', include('ipl_app.urls')),
+     path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('register-franchise/',views.register_franchise, name='register_franchise'),
+   
 ]
